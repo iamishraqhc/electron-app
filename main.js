34a43e834361bd1730891f9b53da66512fce8be5
Ishraq Haider Chowdhury <ishraq.h.c@gmail.com>
2020-12-28 17:55:04 +0100

    Web Content

--- main.js
+++ main.js
@@ -8,6 +8,19 @@ createWindow =() => {
         height: 600
     })
     win.loadFile('index.html')
+    let wc = win.webContents
+    wc.on("dom-ready", () => {
+        console.log("The app is ready now")
+    })
+    wc.on("did-finish-load", () => {
+        console.log("The app is loaded")
+    })
+    wc.on("new-window", () => {
+        console.log("A new window is opened")
+    })
+    wc.on("before-input-event", () => {
+        console.log("new action (Key press/release)")
+    })
 }
 
 app.whenReady().then(createWindow)
